0034c84236cf63fb999b26914cc834e10e8d0f54
Joshua Scott <Joshua.scott.132@gmail.com>
2013-06-04 15:53:21 +1200

    You talkin' to me?

--- Protobition/src/view/Sprite.java
+++ Protobition/src/view/Sprite.java
@@ -7,17 +7,24 @@ public class Sprite extends BufferedImage{
 
 	private double xAnchor;
 	private double yAnchor;
-	
+
+	private double lastScale = 1;
+	private BufferedImage lastImage;
+
 	public Sprite(int x, int y, double xAnchor, double yAnchor) {
 		super(x, y, BufferedImage.TYPE_4BYTE_ABGR);
 		this.xAnchor = xAnchor;
 		this.yAnchor = yAnchor;
+		lastImage = this;
 	}
 
 	public void drawOnto(Graphics2D g, int x, int y, double scale){
-		double width = getWidth() * scale;
-		double height = getHeight() * scale;
-		
-		g.drawImage(this, (int)(x - (width * xAnchor)), (int)(y - (height * yAnchor)), (int)width, (int)height, null);
+		if(Math.abs(scale - lastScale)>0.001){
+			lastImage = new BufferedImage((int)(getWidth()*scale), (int)(getHeight()*scale), BufferedImage.TYPE_4BYTE_ABGR);
+			lastImage.getGraphics().drawImage(this, 0, 0, lastImage.getWidth(), lastImage.getHeight(), null);
+			lastScale = scale;
+		}
+
+		g.drawImage(lastImage, (int)(x - (lastImage.getWidth() * xAnchor)), (int)(y - (lastImage.getHeight() * yAnchor)), lastImage.getWidth(), lastImage.getHeight(), null);
 	}
 }
